b1002369515e56b5378bb452705ffa8c47dfd988
Amelia Haba <amelia.haba347@gmail.com>
2021-07-21 16:14:01 -0400

    add in fetch for characters

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
 // this is where fetches go
+export const fetchCharacters = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3000/characters")
+      .then((response) => response.json())
+      .then((characters) => {
+        console.log(characters);
+        dispatch({ type: "FETCH_CHARACTERS", payload: characters });
+      });
+  };
+};
